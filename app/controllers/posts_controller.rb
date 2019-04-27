8bd1d33dee09b4778bf0a2e42b2c89fd370730e9
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-27 17:50:19 +0100

    ADD: link to posts page from welcome, view improvements.
    listing all posts instead of just one, and associated changes to the controller,
    such as adding the index, and show methods and fixing the wrong bracket i had
    applied to the post_params private method on the controller. Making progress.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,31 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   # when a post is submitted the fields of the post are passed as params, they are saved with this.
   def create
     # creating the object... and whitelisting the controller params title and text... allowing them. 
-    @post = Post.new(post_params)
+    params = post_params
+    @post = Post.new(params)
     # saving the object into the database with a rails method
     @post.save
     # redirecting after its been saved.
     redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
   #looking to keep the params private, so can not be called from outside.
   private
 
   def post_params
-    params.require(:post).permit[:title, :text]
+    params.require(:post).permit(:title, :text)
   end
 end
