525704d8883f16b8c24a520ce428b78427ac878a
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-27 16:46:12 +0100

    REFACTOR: Encapsulate and private params.
    made the post_parms method on the controller to better protect the
    private info in the params and improve the code quality.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,17 @@ class PostsController < ApplicationController
   # when a post is submitted the fields of the post are passed as params, they are saved with this.
   def create
     # creating the object... and whitelisting the controller params title and text... allowing them. 
-    @post = Post.new(params.require(:post).permit[:title, :text])
+    @post = Post.new(post_params)
     # saving the object into the database with a rails method
     @post.save
     # redirecting after its been saved.
     redirect_to @post
+  end
+
+  #looking to keep the params private, so can not be called from outside.
+  private
+
+  def post_params
+    params.require(:post).permit[:title, :text]
   end
 end
